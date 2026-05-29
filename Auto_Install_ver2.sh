d271f8470d16daad47fbf997629e4b93b02006ba
DS1UYM <sejung84@gmail.com>
2026-05-29 16:22:22 +0900

    Update download link for step2_setup script

--- Auto_Install_ver2.sh
+++ Auto_Install_ver2.sh
@@ -28,7 +28,7 @@ sudo ./trixie && \
 sudo apt-get update && \
 sudo apt-get install dvswitch-server -y && \
 grep -qxF "alias dvs='sudo /usr/local/dvs/dvs'" ~/.bashrc || echo "alias dvs='sudo /usr/local/dvs/dvs'" >> ~/.bashrc && \
-wget -O step2_setup https://raw.githubusercontent.com/ds1uym/DVSMU-PHP-NMS/main/Step2_NMS_Setup_ver2.1.run && \
+wget -O step2_setup https://raw.githubusercontent.com/ds1uym/DVSMU-PHP-NMS/main/Step2_NMS_Setup_V2.2.run && \
 sudo chmod +x ./step2_setup && \
 sudo ./step2_setup && \
 source ~/.bashrc
